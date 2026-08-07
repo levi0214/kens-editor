f2a46203a5b17738ab63882d1c907e32eaf59a21
Luyao <zhangluyao.levi@gmail.com>
2026-08-07 16:11:38 +0800

    Move Unmarkdown to editor controls

--- src/App.tsx
+++ src/App.tsx
@@ -1372,28 +1372,6 @@ function App() {
                   </button>
                 </span>
               )}
-              <span
-                className="chrome-tip-wrap"
-                onMouseEnter={() => showHint("unmarkdown")}
-                onMouseLeave={hideHint}
-                onFocus={() => showHint("unmarkdown")}
-                onBlur={hideHint}
-              >
-                <ChromeHint
-                  name="Unmarkdown"
-                  keys={["⇧", "⌘", "R"]}
-                  className="chrome-tip-left"
-                  visible={activeHint === "unmarkdown"}
-                />
-                <button
-                  type="button"
-                  className="statusbar-toggle"
-                  aria-label="Unmarkdown. ⌘⇧R."
-                  onClick={openUnmarkdownConfirm}
-                >
-                  <UnmarkdownIcon className="statusbar-toggle-icon" />
-                </button>
-              </span>
             </div>
           )}
           {saveError && (
@@ -1403,6 +1381,28 @@ function App() {
           )}
         </div>
         <div className="statusbar-controls">
+          <span
+            className="chrome-tip-wrap"
+            onMouseEnter={() => showHint("unmarkdown")}
+            onMouseLeave={hideHint}
+            onFocus={() => showHint("unmarkdown")}
+            onBlur={hideHint}
+          >
+            <ChromeHint
+              name="Unmarkdown"
+              keys={["⇧", "⌘", "R"]}
+              className="chrome-tip-right"
+              visible={activeHint === "unmarkdown"}
+            />
+            <button
+              type="button"
+              className="statusbar-toggle"
+              aria-label="Unmarkdown. ⌘⇧R."
+              onClick={openUnmarkdownConfirm}
+            >
+              <UnmarkdownIcon className="statusbar-toggle-icon" />
+            </button>
+          </span>
           <span
             className="chrome-tip-wrap"
             onMouseEnter={() => showHint("font")}
