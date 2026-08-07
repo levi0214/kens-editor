586c032c60d49955566de4db0180c13947e666f4
Luyao <zhangluyao.levi@gmail.com>
2026-08-07 16:05:21 +0800

    Shrink the versions icon

--- src/statusBarIcons.tsx
+++ src/statusBarIcons.tsx
@@ -122,7 +122,7 @@ export function ImagesIcon(props: IconProps) {
 
 export function VersionsIcon(props: IconProps) {
   return (
-    <Icon viewBox="0 0 24 24" strokeWidth="2" strokeLinejoin="round" {...props}>
+    <Icon viewBox="-2 -2 28 28" strokeWidth="2" strokeLinejoin="round" {...props}>
       <path d="M16 22h2a2 2 0 0 0 2-2V8a2.4 2.4 0 0 0-.706-1.706l-3.588-3.588A2.4 2.4 0 0 0 14 2H6a2 2 0 0 0-2 2v2.85" />
       <path d="M14 2v5a1 1 0 0 0 1 1h5" />
       <path d="M8 14v2.2l1.6 1" />
